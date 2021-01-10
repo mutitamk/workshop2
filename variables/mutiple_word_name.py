bb8b6a2cb6c17f6a7a4ff73885c9455d06c5ade5
mutitamk <mutitar091@gmail.com>
2021-01-10 11:09:24 +0700

    Add new 3 files

---
+++ variables/mutiple_word_name.py
@@ -0,0 +1,8 @@
+# Camel Case
+myVariableName = "Mutita Kunghae"
+
+# Pascal Case
+MyVariableName = "Mutita Kunghae"
+
+# Snake Case
+my_variable_name = "Mutita Kunghae"
---
+++ variables/name.py
@@ -0,0 +1,11 @@
+myvar = "mate"
+my_var ="mate"
+_my_var = "mate"
+myVar = "mate"
+MYVAR = "mate"
+myvar2 = "mate"
+
+#ไม่สามารถประกาศได้
+2myvar = "mate"
+my-var = "mate"
+my var = "mate"
