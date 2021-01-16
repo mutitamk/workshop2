23b03b9282f356e5716aa4286969cb2f0185f5c5
mutitamk <mutitar091@gmail.com>
2021-01-16 11:39:33 +0700

    Add a folder and add New 7 files type , string , modify , concatenati , format , f_string and string_exercise_1

---
+++ string/f_string.py
@@ -0,0 +1,6 @@
+name = "mate"
+age = 21
+
+result = f"My name is {name}, and I am {age}"
+
+print("result", result)  # Output: "My name is mate, and I am 21"
---
+++ string/format.py
@@ -0,0 +1,6 @@
+age = 21
+txt = "My name is mate, and I am {}"
+
+result = txt.format(age)
+
+print("result", result)  # Output: "My name is mate, and I am 21"
